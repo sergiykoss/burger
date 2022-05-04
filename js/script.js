132f960760a6cf064c914bb1bb645b7ce632a5d5
SergiyK <sergiy.kostiv@gmail.com>
2022-05-04 21:12:30 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+(function () {
+    const burder = document .querySelector('.burger');
+    burder.addEventListener('click', () => {
+        burder.classList.toggle('burger__active')
+    });
+}())
